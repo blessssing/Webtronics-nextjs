6017cc5d711afe3678bc3d932790731940356d69
blessssing <betterricherhigher@gmail.com>
2023-02-08 18:53:28 +0300

    update next.config.js, .gitignore, package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/webtronics-nextjs",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
